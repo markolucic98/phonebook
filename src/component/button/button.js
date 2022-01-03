2b810ae74690b83646d6a4ecdfcd6ac5e121aebb
Marko Lucic <lucicmarko48@gmail.com>
2022-01-03 19:39:51 +0100

    Completed nav bar component and added MUI to the project

---
+++ src/component/button/button.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+const ButtonContact = () => {
+  return (
+    <Button
+      variant="outlined"
+      size="medium"
+      style={{
+        backgroundColor: "rgb(255, 255, 255)",
+        color: "black",
+        borderRadius: "15px",
+      }}
+    >
+      Kontakt
+    </Button>
+  );
+};
+export default ButtonContact;
--- src/component/nav/Nav.js
+++ src/component/nav/Nav.js
@@ -1,14 +1,22 @@
 import React from "react";
 import styles from "../nav/Nav.module.css";
+import img1 from "../../img/icon-address.png";
+import ButtonContact from "../button/button";
 
 const Nav = () => {
   return (
     <div className={styles.Nav}>
+      <div className={styles.navLeft}>
+        <img src={img1} className={styles.logoimg} />
+        <h3>Kontakti</h3>
+      </div>
       <div className={styles.pages}>
         <ul>
           <li>Pocetna</li>
           <li>O nama</li>
-          <li>Button</li>
+          <li>
+            <ButtonContact />
+          </li>
         </ul>
       </div>
     </div>
