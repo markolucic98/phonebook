e96a8df3dbd1f7994c1082ecb10662a71490b055
Marko Lucic <lucicmarko48@gmail.com>
2022-01-13 00:00:18 +0100

    Creating About page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Nav from "../src/component/nav/Nav";
 import "./App.css";
-import HomePage from "./component/homePage/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <HomePage />
     </div>
   );
 }
--- src/component/nav/Nav.js
+++ src/component/nav/Nav.js
@@ -2,24 +2,40 @@ import React from "react";
 import styles from "../nav/Nav.module.css";
 import img1 from "../../img/icon-address.png";
 import ButtonContact from "../button/button";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "../homePage/HomePage";
+import About from "../about/About";
 const Nav = () => {
   return (
-    <div className={styles.Nav}>
-      <div className={styles.navLeft}>
-        <img src={img1} className={styles.logoimg} />
-        <h3>Kontakti</h3>
+    <Router>
+      <div className={styles.Nav}>
+        <div className={styles.navLeft}>
+          <img src={img1} className={styles.logoimg} />
+          <h3>Kontakti</h3>
+        </div>
+        <div className={styles.pages}>
+          <ul>
+            <li>
+              <Link className={styles.link} to="/">
+                Pocetna
+              </Link>
+            </li>
+            <li>
+              <Link className={styles.link} to="/about">
+                O nama
+              </Link>
+            </li>
+            <li>
+              <ButtonContact />
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className={styles.pages}>
-        <ul>
-          <li>Pocetna</li>
-          <li>O nama</li>
-          <li>
-            <ButtonContact />
-          </li>
-        </ul>
-      </div>
-    </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 };
 
